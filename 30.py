5d9f6b132f0bc051b5e393a538c50905d59bc418
AYUSH <ayushchaturvedi888@gmail.com>
2022-05-29 19:59:29 +0530

    30-Commit

---
+++ 30.py
@@ -0,0 +1,12 @@
+#Q-10 # Python program explaining floor_divide() function.
+import numpy as np
+  
+# input_array
+arr1 = [2, 2, 2, 2, 2]
+arr2 = [2, 3, 4, 5, 6]
+print ("arr1         : ", arr1)
+print ("arr1         : ", arr2)
+  
+# output_array
+out = np.floor_divide(arr1, arr2)
+print ("\nOutput array : ", out)
