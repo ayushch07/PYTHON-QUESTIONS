469f71edfb4845576eb8755b509d2c87dc41e9e9
AYUSH <ayushchaturvedi888@gmail.com>
2022-05-29 19:56:24 +0530

    18-Commit

---
+++ 18.py
@@ -0,0 +1,16 @@
+#Q-8 Python dictionary with keys having multiple inputs.
+import random as rn
+ 
+# creating an empty dictionary
+dict = {}
+ 
+# Insert first triplet in dictionary
+x, y, z = 10, 20, 30
+dict[x, y, z] = x + y - z;
+ 
+# Insert second triplet in dictionary
+x, y, z = 5, 2, 4
+dict[x, y, z] = x + y - z;
+ 
+# print the dictionary
+print(dict)
