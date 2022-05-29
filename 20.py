8720fc752a0629781cbd201e899c21310f81306d
AYUSH <ayushchaturvedi888@gmail.com>
2022-05-29 19:56:49 +0530

    20-Commit

---
+++ 20.py
@@ -0,0 +1,12 @@
+#Q-10 Add a key:value pair to dictionary in Python.
+dict = {'key1':'geeks', 'key2':'for'} 
+print("Current Dict is: ", dict) 
+    
+# using the subscript notation 
+# Dictionary_Name[New_Key_Name] = New_Key_Value 
+  
+dict['key3'] = 'Geeks'
+dict['key4'] = 'is'
+dict['key5'] = 'portal'
+dict['key6'] = 'Computer'
+print("Updated Dict is: ", dict)
