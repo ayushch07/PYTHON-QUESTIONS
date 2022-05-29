dec3af8f2057af3ed2b759c3f0be4d97e15d9802
AYUSH <ayushchaturvedi888@gmail.com>
2022-05-29 19:55:55 +0530

    16-Commit

---
+++ 16.py
@@ -0,0 +1,19 @@
+#Q-6 Sum list of dictionaries with same key.
+from operator import itemgetter
+  
+# Initialising list of dictionary
+ini_dict = [{'a':5, 'b':10, 'c':90},
+            {'a':45, 'b':78}, 
+            {'a':90, 'c':10}]
+  
+# printing initial dictionary
+print ("initial dictionary", str(ini_dict))
+  
+# sum the values with same keys
+result = {}
+for d in ini_dict:
+    for k in d.keys():
+        result[k] = result.get(k, 0) + d[k]
+  
+  
+print("resultant dictionary : ", str(result))
