29d40a25ebc5cd697adcefe0ce87c5585d6e41ec
AYUSH <ayushchaturvedi888@gmail.com>
2022-05-29 19:58:32 +0530

    26-Commit

---
+++ 26.py
@@ -0,0 +1,6 @@
+# Q-6 Python Programming illustrating numpy.arange method.
+import numpy as geek
+ 
+print("A\n", geek.arange(4).reshape(2, 2), "\n")
+print("A\n", geek.arange(4, 10), "\n")
+print("A\n", geek.arange(4, 20, 3), "\n")
