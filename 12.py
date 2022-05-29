32c60cabff0fd3daa3d99a57bc0952d4764fdb1a
AYUSH <ayushchaturvedi888@gmail.com>
2022-05-29 19:54:51 +0530

    12-Commit

---
+++ 12.py
@@ -0,0 +1,24 @@
+#Q-2 To sort list of dictionaries by values in Python – Using lambda function.
+lis = [{ "name" : "Nandini", "age" : 20},
+{ "name" : "Manjeet", "age" : 20 },
+{ "name" : "Nikhil" , "age" : 19 }]
+ 
+# using sorted and lambda to print list sorted
+# by age
+print ("The list printed sorting by age: ")
+print (sorted(lis, key = lambda i: i['age']))
+ 
+print ("\r")
+ 
+# using sorted and lambda to print list sorted
+# by both age and name. Notice that "Manjeet"
+# now comes before "Nandini"
+print ("The list printed sorting by age and name: ")
+print (sorted(lis, key = lambda i: (i['age'], i['name'])))
+ 
+print ("\r")
+ 
+# using sorted and lambda to print list sorted
+# by age in descending order
+print ("The list printed sorting by age in descending order: ")
+print (sorted(lis, key = lambda i: i['age'],reverse=True))
